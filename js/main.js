20f185102d8b51866d6601e9eca19098af1cd21f
rdeepak2002 <rdeepak2002@gmail.com>
2021-10-02 18:11:16 -0400

    added music

--- js/main.js
+++ js/main.js
@@ -43,6 +43,6 @@ const bgColor = '#dfe0df';
 let music = '../resources/music.mp3';
 
 // uncomment to mute music:
-// music = undefined;
+music = undefined;
 
 new Game(GAME_WIDTH, GAME_HEIGHT, gameLoop, bgColor, music).init();
